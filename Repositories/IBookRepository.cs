7a4b72222d39a8212d13d50978b3e163183d761f
Enock Lalusya <E.Lalusya@azania.co.tz>
2021-06-23 16:19:27 +0300

    Add project files.

---
+++ Repositories/IBookRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.DataModels;
+
+namespace WebAPI.Repositories
+{
+    //this contains all operation to be performed in data base
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+
+        Task<Book> Get(int id);
+
+        Task<Book> Create(Book book);
+
+        Task Update(Book book);
+
+        Task Delete(int id);
+    }
+}
